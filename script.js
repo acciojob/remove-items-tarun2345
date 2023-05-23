41b315659fbc88e6356517e83f2bb98cb56e0481
tarun2345 <94225512+tarun2345@users.noreply.github.com>
2023-05-23 13:44:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let btn = document.getElementById('btn');
+
+btn.addEventListener("click",()=>{
+    const selectElement = document.getElementById('colorSelect');
+    const selectedOption = selectElement.options[selectElement.selectedIndex];
+  
+    if (selectedOption) {
+      selectElement.removeChild(selectedOption);
+    }
+})
